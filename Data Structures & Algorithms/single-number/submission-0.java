4374f05ea64832bc7bf42f5d8c06d43731c51756
NAZMIN SHAIK <nazmin.sk67@gmail.com>
2026-07-08 19:28:30 +0530

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Map<Integer,Integer> m=new HashMap<>();
+        for(int num:nums){
+            m.put(num,m.getOrDefault(num,0)+1);
+        }
+        for(Map.Entry<Integer,Integer> entry:m.entrySet()){
+            if(entry.getValue()==1){
+                return entry.getKey();
+            }
+        }
+        return -1;
+    }
+}
